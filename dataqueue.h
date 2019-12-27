f7ec94744ec1ada741e119d99e2545e9a9141f06
arsenycherenkov <senyacherenkov@yandex.ru>
2019-12-27 16:44:49 +0300

    migration to new repo

---
+++ dataqueue.h
@@ -0,0 +1,316 @@
+#pragma once
+#include <vector>
+#include <cstddef>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <condition_variable>
+#include <type_traits>
+#include <atomic>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+
+namespace {
+    template<typename T, typename... Args>
+    std::unique_ptr<T> make_unique(Args&&... args) {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+
+    template<typename T, typename = typename std::enable_if<std::is_pod<T>::value>::type>
+    std::ostream& write(const T& value, std::ofstream& ifs)
+    {        
+        return ifs.write(reinterpret_cast<const char*>(&value), sizeof (T));
+    }
+
+    template<typename T>
+    typename std::enable_if<!std::is_pod<T>::value, std::ostream&>::type
+    write(const T& value, std::ofstream& ifs)
+    {
+        return value.serialize(ifs);
+    }
+
+    template<typename T, typename = typename std::enable_if<std::is_pod<T>::value>::type>
+    bool read(T& value, std::ifstream& ofs)
+    {        
+        if(!ofs.read(reinterpret_cast<char*>(&value), sizeof(T)))
+            return false;
+        return true;
+    }
+
+    template<typename T>
+    typename std::enable_if<!std::is_pod<T>::value, bool>::type
+    read(T& value, std::ifstream& ofs)
+    {        
+        return value.deserialize(ofs);
+    }
+
+    decltype(std::chrono::seconds().count()) getSecondsSinceEpoch()
+    {
+        // get the current time
+        const auto now     = std::chrono::system_clock::now();
+
+        // transform the time into a duration since the epoch
+        const auto epoch   = now.time_since_epoch();
+
+        // cast the duration into seconds
+        const auto seconds = std::chrono::duration_cast<std::chrono::seconds>(epoch);
+
+        // return the number of seconds
+        return seconds.count();
+    }
+}
+
+namespace threadsafe {
+
+template <typename T, std::size_t QUEUE_SIZE = 256,
+          typename = typename std::enable_if<std::is_nothrow_copy_constructible<T>::value>::type,
+          typename = typename std::enable_if<std::is_default_constructible<T>::value>::type>
+class queue{
+private:
+    struct node
+    {
+        std::shared_ptr<T> data {nullptr};
+        std::unique_ptr<node> next {nullptr};
+        int number = 0;
+    };
+
+public:
+    queue():
+        m_head(make_unique<node>()), m_tail(m_head.get())
+    {}
+
+    queue(const queue& other) = delete;
+    queue& operator= (const queue& other) = delete;
+
+    ~queue() {
+        while(tryPop());
+    }
+
+    bool tryPush(const T& newItem) {
+        std::shared_ptr<T> newData = std::make_shared<T>(newItem);
+        std::unique_ptr<node> newVertex(make_unique<node>());
+
+        {
+            std::lock_guard<std::mutex> tailLock(m_tailMutex);
+
+            if(m_tail->number > QUEUE_SIZE)
+                return false;
+
+            pushToTail(newData, std::move(newVertex));
+        }
+
+        m_dataAwaiting.notify_one();
+        return true;
+    }
+
+
+    void waitPush(const T& newItem) {
+        std::shared_ptr<T> newData = std::make_shared<T>(newItem);
+        std::unique_ptr<node> newVertex(make_unique<node>());
+
+        {
+            std::unique_lock<std::mutex> tailLock(waitForRoom());
+
+            if(m_stopWaitForRoom.exchange(false, std::memory_order_acq_rel)) {
+                return;
+            }
+
+            pushToTail(newData, std::move(newVertex));
+        }
+
+        m_dataAwaiting.notify_one();
+    }
+
+    bool tryPop(T& item) {
+        std::unique_ptr<node> const oldHead = tryPopHead(item);
+        m_roomAwaiting.notify_one();
+        return oldHead.get();
+    }
+
+    std::shared_ptr<T> tryPop() {
+        std::unique_ptr<node> const oldHead = tryPopHead();
+        m_roomAwaiting.notify_one();
+        return oldHead? oldHead->data: std::shared_ptr<T>();
+    }
+
+    void waitPop(T& item) {
+        std::unique_ptr<node> const oldHead = waitPopHead(item);
+        if(!oldHead.get())
+            return;
+        m_roomAwaiting.notify_one();
+    }
+
+    std::shared_ptr<T> waitPop() {
+        std::unique_ptr<node> const oldHead = waitPopHead();
+        if(!oldHead.get())
+            return std::shared_ptr<T>();
+        m_roomAwaiting.notify_one();
+        return oldHead->data;
+    }    
+
+    bool empty() {
+        std::lock_guard<std::mutex> headLock(m_headMutex);
+        return (m_head.get() == getTail());
+    }
+
+    bool full() {
+        return (getTail()->number == QUEUE_SIZE);
+    }
+
+    void stopWaiting(){
+        if(empty()) {
+            m_stopWaitForData.store(true, std::memory_order_release);
+            m_dataAwaiting.notify_all();
+        } else if(full()) {
+            m_stopWaitForRoom.store(true, std::memory_order_release);
+            m_roomAwaiting.notify_all();
+        }
+    }
+
+    std::string storeToDisk(const char* name) {
+
+        std::string filename(name);
+        filename += std::to_string(getSecondsSinceEpoch());        
+
+        std::ofstream ifs(filename, std::ios_base::out | std::ios::binary);
+        if(ifs.is_open())
+        {
+            std::lock(m_headMutex, m_tailMutex);
+            std::unique_lock<std::mutex> headLock(m_headMutex, std::adopt_lock);
+            std::unique_lock<std::mutex> tailLock(m_tailMutex, std::adopt_lock);
+
+            if(m_head.get() != m_tail)
+            {
+                node* temp = m_head.get();
+                while (temp != m_tail) {
+                    T& data = *temp->data;
+                    write(data, ifs);
+                    temp = temp->next.get();
+                }
+            }
+        }
+
+        return filename;
+    }
+
+    bool tryReadFromDisk(const char* filename) {
+        std::ifstream ofs(filename, std::ios_base::in | std::ios::binary);
+        if(ofs.is_open())
+        {
+            while(!ofs.eof())
+            {
+                T value;
+                bool eof = ofs.eof();
+                if(!read<T>(value, ofs))
+                    if(ofs)
+                        return false;
+                    else
+                        break;
+                else if(!tryPush(value))
+                    return false;
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+private:
+    node* getTail()
+    {
+        std::lock_guard<std::mutex> tailLock(m_tailMutex);
+        return m_tail;
+    }
+
+    /*****POP AREA*****/
+    std::unique_ptr<node> popHead()
+    {
+        std::unique_ptr<node> oldHead = std::move(m_head);
+        m_head = std::move(oldHead->next);
+        return oldHead;
+    }
+
+    std::unique_ptr<node> tryPopHead()
+    {
+        std::unique_lock<std::mutex> headLock(m_headMutex);
+        if(m_head.get() == getTail())
+        {
+            return std::unique_ptr<node>();
+        }
+        getTail()->number--;
+        return popHead();
+    }
+
+    std::unique_ptr<node> tryPopHead(T& item)
+    {
+        std::unique_lock<std::mutex> headLock(m_headMutex);
+        if(m_head.get() == getTail())
+        {
+            return std::unique_ptr<node>();
+        }
+        getTail()->number--;
+        item = std::move(*m_head->data);
+        return popHead();
+    }
+
+    std::unique_lock<std::mutex> waitForData()
+    {
+        std::unique_lock<std::mutex> headLock(m_headMutex);
+        m_dataAwaiting.wait(headLock, [&](){ return (m_head.get() != getTail()) ||
+                    m_stopWaitForData.load(std::memory_order_acquire); });
+        return headLock;
+    }
+
+    std::unique_ptr<node> waitPopHead()
+    {
+        std::unique_lock<std::mutex> headLock(waitForData());
+        if(m_stopWaitForData.exchange(false, std::memory_order_acq_rel))
+            return std::unique_ptr<node>();
+        getTail()->number--;
+        return popHead();
+    }
+
+    std::unique_ptr<node> waitPopHead(T& item)
+    {
+        std::unique_lock<std::mutex> headLock(waitForData());
+        if(m_stopWaitForData.exchange(false, std::memory_order_acq_rel))
+            return std::unique_ptr<node>();
+        getTail()->number--;
+        item = std::move(*m_head->data);
+        return popHead();
+    }
+
+    /*****POP AREA END*****/
+
+    /*****PUSH AREA*****/
+    std::unique_lock<std::mutex> waitForRoom()
+    {
+        std::unique_lock<std::mutex> tailLock(m_tailMutex);
+        m_roomAwaiting.wait(tailLock, [&](){ return (m_tail->number < QUEUE_SIZE) ||
+                    m_stopWaitForRoom.load(std::memory_order_acquire); });
+        return tailLock;
+    }
+
+    void pushToTail(std::shared_ptr<T>& newData, std::unique_ptr<node> newVertex)
+    {
+        m_tail->data = newData;
+        node* const newTail = newVertex.get();
+        m_tail->next = std::move(newVertex);
+        newTail->number = ++m_tail->number;
+        m_tail = newTail;        
+    }
+    /*****PUSH AREA END*****/
+private:
+    std::atomic_bool        m_stopWaitForData{false};
+    std::atomic_bool        m_stopWaitForRoom{false};
+    std::mutex              m_headMutex;
+    std::unique_ptr<node>   m_head;
+    std::mutex              m_tailMutex;
+    node*                   m_tail;
+    std::condition_variable m_dataAwaiting;
+    std::condition_variable m_roomAwaiting;
+};
+} // namespace threadsafe
---
+++ main.cpp
@@ -0,0 +1,379 @@
+#include "dataqueue.h"
+#include <string>
+#include <ctime>
+#include <chrono>
+#include <random>
+#include <iterator>
+
+constexpr int QUEUE_SIZE = 10;
+constexpr int NUMBER_OF_ELEMENTS = 50;
+constexpr int NUMBER_OF_ELEMENTS_FOR_STOP_TEST = QUEUE_SIZE + 1;
+
+#define BOOST_TEST_MODULE test_main
+
+#include <boost/test/included/unit_test.hpp>
+
+void unpredictableDelay(int extra = 0) {
+    std::random_device rand_dev;
+    std::default_random_engine generator(rand_dev());
+    std::uniform_int_distribution<int> distribution(0, 10);
+
+    std::chrono::milliseconds delay{distribution(generator) + extra};
+    std::this_thread::sleep_for(delay);
+}
+
+using namespace boost::unit_test;
+BOOST_AUTO_TEST_SUITE(test_suite_main)
+
+BOOST_AUTO_TEST_CASE(one_writer_one_reader_tryPush_tryPop_first_overload)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queue;
+
+    std::thread writer([&]() {
+                    for (int j = 0; j < QUEUE_SIZE; ++j) {
+                        BOOST_CHECK_MESSAGE(queue.tryPush(j), "value wasn't pushed");
+                    }
+                    unpredictableDelay(10);
+              });
+
+    std::thread reader([&]() {
+        int element;
+        int counter = 0;
+        unpredictableDelay(10);
+        BOOST_CHECK_MESSAGE(!queue.empty(), "queue is empty");
+
+        while (!queue.empty()) {
+            BOOST_CHECK_MESSAGE(queue.tryPop(element), "value wasn't popped");
+            BOOST_CHECK_MESSAGE(element == counter, "Expected value " << counter << "; real value " << element);
+            ++counter;
+        }
+        BOOST_CHECK_MESSAGE(!queue.tryPop(element), "Expected that queue has no element to be popped");
+        BOOST_CHECK_MESSAGE(queue.empty(), "Expected that queue is empty");
+    });
+
+    writer.join();
+    reader.join();
+}
+
+BOOST_AUTO_TEST_CASE(one_writer_one_reader_tryPush_tryPop_second_overload)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queue;
+
+    std::thread writer([&]() {
+                    for (int j = 0; j < QUEUE_SIZE; ++j) {
+                        BOOST_CHECK_MESSAGE(queue.tryPush(j), "value wasn't pushed");
+                    }
+                    unpredictableDelay(10);
+              });
+
+    std::thread reader([&]() {
+        int counter = 0;
+        unpredictableDelay(10);
+        BOOST_CHECK_MESSAGE(!queue.empty(), "queue is empty");
+
+        while (!queue.empty()) {
+            auto element = queue.tryPop();
+            BOOST_CHECK_MESSAGE(element.get() != nullptr, "value wasn't popped");
+            BOOST_CHECK_MESSAGE(*element == counter, "Expected value " << counter << "; real value " << element);
+            ++counter;
+        }
+        auto element = queue.tryPop();
+        BOOST_CHECK_MESSAGE(element.get() == nullptr, "Expected that queue has no element to be popped");
+        BOOST_CHECK_MESSAGE(queue.empty(), "Expected that queue is empty");
+    });
+
+    writer.join();
+    reader.join();
+}
+
+BOOST_AUTO_TEST_CASE(one_writer_one_reader_waitPush_waitPop_first_overload)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queue;
+
+    std::thread writer([&]() {
+                    for (int j = 0; j < NUMBER_OF_ELEMENTS; ++j) {
+                        unpredictableDelay(10);
+                        queue.waitPush(j);
+                    }
+
+              });
+
+    std::thread reader([&]() {
+        int element;
+        for (int j = 0; j < NUMBER_OF_ELEMENTS;) {
+            unpredictableDelay();
+            queue.waitPop(element);
+            BOOST_CHECK_MESSAGE(element == j, "Expected value " << j << "; real value " << element);
+            ++j;
+        }
+        BOOST_CHECK_MESSAGE(!queue.tryPop(element), "Expected that queue is empty");
+    });
+
+    writer.join();
+    reader.join();
+}
+
+
+BOOST_AUTO_TEST_CASE(one_writer_one_reader_waitPush_waitPop_second_overload)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queue;
+
+    std::thread writer([&]() {
+                    for (int j = 0; j < NUMBER_OF_ELEMENTS; ++j) {
+                        unpredictableDelay(10);
+                        queue.waitPush(j);
+                    }
+
+              });
+
+    std::thread reader([&]() {
+        for (int j = 0; j < NUMBER_OF_ELEMENTS;) {
+            unpredictableDelay();
+            auto element = queue.waitPop();
+            BOOST_CHECK_MESSAGE(*element == j, "Expected value " << j << "; real value " << element);
+            ++j;
+        }
+        auto element = queue.tryPop();
+        BOOST_CHECK_MESSAGE(element.get() == nullptr, "Expected that queue is empty");
+    });
+
+    writer.join();
+    reader.join();
+}
+
+BOOST_AUTO_TEST_CASE(one_thread_writer_waits_because_full_other_thread_stops)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queue;
+
+    std::thread writer([&]() {
+        for (int j = 0; j < NUMBER_OF_ELEMENTS_FOR_STOP_TEST; ++j) {
+            queue.waitPush(j);
+        }
+
+        BOOST_CHECK_MESSAGE(queue.full(), "queue must be full");
+    });
+
+    std::thread breaker([&]() {
+        std::chrono::milliseconds delay{10};
+        std::this_thread::sleep_for(delay);
+
+        bool isFull = false;
+        isFull = queue.full();
+        BOOST_CHECK_MESSAGE(isFull, "queue must be full");
+
+        if(isFull)
+            queue.stopWaiting();
+    });
+
+    writer.join();
+    breaker.join();
+}
+
+BOOST_AUTO_TEST_CASE(one_thread_writer_waits_because_empty_other_thread_stops)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queue;
+
+    std::thread breaker([&]() {
+        std::chrono::milliseconds delay{100};
+        std::this_thread::sleep_for(delay);
+
+        bool isEmpty = false;
+        isEmpty = queue.empty();
+        BOOST_CHECK_MESSAGE(isEmpty, "queue must be empty");
+
+        if(isEmpty)
+            queue.stopWaiting();
+    });
+
+    std::thread reader([&]() {
+        int element;
+        queue.waitPop(element);
+
+        BOOST_CHECK_MESSAGE(queue.empty(), "queue must be empty");
+    });
+
+    breaker.join();
+    reader.join();
+}
+
+BOOST_AUTO_TEST_CASE(two_threads_are_writers_other_one_is_reader)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queue;
+
+    std::thread writer1([&]() {
+                    for (int j = 0; j < NUMBER_OF_ELEMENTS; ++j) {
+                        unpredictableDelay(5);
+                        queue.waitPush(j);
+                    }
+
+              });
+
+    std::thread writer2([&]() {
+                    for (int j = 0; j < NUMBER_OF_ELEMENTS; ++j) {
+                        unpredictableDelay(5);
+                        queue.waitPush(j);
+                    }
+
+              });
+
+    std::thread reader([&]() {
+        int element;
+        std::vector<int> poppedData;
+        poppedData.reserve(2*NUMBER_OF_ELEMENTS);
+
+        unpredictableDelay(200);
+        BOOST_CHECK_MESSAGE(queue.full(), "queue must be full");
+
+        while(!queue.empty()) {
+            unpredictableDelay(10);
+            queue.waitPop(element);
+            poppedData.push_back(element);
+        }
+
+        BOOST_CHECK_MESSAGE(poppedData.size() == 2*NUMBER_OF_ELEMENTS, "vector's size has wrong amount of elements");
+    });
+
+    writer1.join();
+    writer2.join();
+    reader.join();
+}
+
+BOOST_AUTO_TEST_CASE(store_and_try_read_from_disk_fundamental_int_type)
+{
+    threadsafe::queue<int, QUEUE_SIZE> queueForStore;
+    threadsafe::queue<int, QUEUE_SIZE> queueForRead;
+    std::string filename("queue_snapshot_");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j)
+        BOOST_CHECK_MESSAGE(queueForStore.tryPush(j), "cannot push data into queue");
+
+    filename = queueForStore.storeToDisk(filename.c_str());
+    bool isReadOk = queueForRead.tryReadFromDisk(filename.c_str());
+    BOOST_CHECK_MESSAGE(isReadOk, "reading data from disk was failed");
+    BOOST_CHECK_MESSAGE(queueForRead.full(), "queue for reading was filled wrong");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j)
+        BOOST_CHECK_MESSAGE(*queueForStore.tryPop() == *queueForRead.tryPop(), "queues are not identical");
+}
+
+BOOST_AUTO_TEST_CASE(store_and_try_read_from_disk_fundamental_double_type)
+{
+    threadsafe::queue<double, QUEUE_SIZE> queueForStore;
+    threadsafe::queue<double, QUEUE_SIZE> queueForRead;
+    std::string filename("queue_snapshot_");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j)
+        BOOST_CHECK_MESSAGE(queueForStore.tryPush(j), "cannot push data into queue");
+
+    filename = queueForStore.storeToDisk(filename.c_str());
+    bool isReadOk = queueForRead.tryReadFromDisk(filename.c_str());
+    BOOST_CHECK_MESSAGE(isReadOk, "reading data from disk was failed");
+    BOOST_CHECK_MESSAGE(queueForRead.full(), "queue for reading was filled wrong");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j)
+        BOOST_CHECK_MESSAGE(*queueForStore.tryPop() == *queueForRead.tryPop(), "queues are not identical");
+}
+
+BOOST_AUTO_TEST_CASE(store_and_try_read_from_disk_fundamental_user_type_pod)
+{
+    struct userType{
+        userType() = default;
+
+        uint32_t member1;
+        uint32_t member2;
+        double member3;
+
+        bool operator == (const userType& rhs) const
+        {
+            return member1 == rhs.member1 &&
+                   member2 == rhs.member2 &&
+                   member3 == rhs.member3;
+        }
+    };
+
+    threadsafe::queue<userType, QUEUE_SIZE> queueForStore;
+    threadsafe::queue<userType, QUEUE_SIZE> queueForRead;
+    std::string filename("queue_snapshot_");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j) {
+        userType instance;
+        instance.member1 = static_cast<uint32_t>(j);
+        instance.member2 = static_cast<uint32_t>(j + 1);
+        instance.member3 = j + 0.5;
+
+        BOOST_CHECK_MESSAGE(queueForStore.tryPush(instance), "cannot push data into queue");
+    }
+
+    filename = queueForStore.storeToDisk(filename.c_str());
+    bool isReadOk = queueForRead.tryReadFromDisk(filename.c_str());
+    BOOST_CHECK_MESSAGE(isReadOk, "reading data from disk was failed");
+    BOOST_CHECK_MESSAGE(queueForRead.full(), "queue for reading was filled wrong");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j)
+        BOOST_CHECK_MESSAGE(*queueForStore.tryPop() == *queueForRead.tryPop(), "queues are not identical");
+}
+
+/*
+ * userType which is not a POD type should provide two methods:
+ * serialize() and deserialize()
+*/
+BOOST_AUTO_TEST_CASE(store_and_try_read_from_disk_fundamental_user_type_not_pod)
+{
+    struct userType{
+        userType() = default;
+        userType(uint32_t m1, uint32_t m2, double m3):
+            member1(m1),
+            member2(m2),
+            member3(m3)
+        {}
+
+        ~userType(){}
+
+        uint32_t member1;
+        uint32_t member2;
+        double member3;
+
+        std::ostream& serialize(std::ofstream& ifs) const
+        {
+            ifs.write(reinterpret_cast<const char*>(&member1), sizeof (uint32_t));
+            ifs.write(reinterpret_cast<const char*>(&member2), sizeof (uint32_t));
+            ifs.write(reinterpret_cast<const char*>(&member3), sizeof (double));
+            return ifs;
+        }
+
+        bool deserialize(std::ifstream& ofs)
+        {
+            ofs.read(reinterpret_cast<char*>(&member1), sizeof(uint32_t));
+            ofs.read(reinterpret_cast<char*>(&member2), sizeof(uint32_t));
+            ofs.read(reinterpret_cast<char*>(&member3), sizeof(double));
+            if(ofs)
+                return true;
+            return false;
+        }
+
+        bool operator == (const userType& rhs) const
+        {
+            return member1 == rhs.member1 &&
+                   member2 == rhs.member2 &&
+                   member3 == rhs.member3;
+        }
+    };
+
+    threadsafe::queue<userType, QUEUE_SIZE> queueForStore;
+    threadsafe::queue<userType, QUEUE_SIZE> queueForRead;
+    std::string filename("queue_snapshot_");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j) {
+        userType instance(static_cast<uint32_t>(j), static_cast<uint32_t>(j + 1), j + 0.5);
+        BOOST_CHECK_MESSAGE(queueForStore.tryPush(instance), "cannot push data into queue");
+    }
+
+    filename = queueForStore.storeToDisk(filename.c_str());
+    bool isReadOk = queueForRead.tryReadFromDisk(filename.c_str());
+    BOOST_CHECK_MESSAGE(isReadOk, "reading data from disk was failed");
+    BOOST_CHECK_MESSAGE(queueForRead.full(), "queue for reading was filled wrong");
+
+    for (int j = 0; j < QUEUE_SIZE; ++j)
+        BOOST_CHECK_MESSAGE(*queueForStore.tryPop() == *queueForRead.tryPop(), "queues are not identical");
+}
+BOOST_AUTO_TEST_SUITE_END()
